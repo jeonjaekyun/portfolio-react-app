70a3f01c7e2a2cee6c9d464990c3fd4f7ec67c71
jeonjaekyun <jkyun01@gmail.com>
2020-07-14 22:37:44 +0900

    LandingPage 구현

--- src/components/views/LandingPage/LandingPage.js
+++ src/components/views/LandingPage/LandingPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Carousel} from 'antd';
+import {Carousel, Row, Col} from 'antd';
 import './LandingPage.css'
 
 function LandingPage(props) {
@@ -20,16 +20,58 @@ function LandingPage(props) {
         <div style={{ width: '100%', margin: '0' }}>
             <div style={{ width: '85%', margin: '1rem auto' }}>
                 <Carousel autoplay>
-                    <div className="home" onClick={onHomeHandler}>
+                    <div id="home" onClick={onHomeHandler}>
                         <span>Backend 개발자를 희망하는 신입 개발자 입니다.</span>
                     </div>
-                    <div className="profile" onClick={onProfileHandler}>
+                    <div id="profile" onClick={onProfileHandler}>
                         <span>Profile</span>
                     </div>
-                    <div className="portfolio" onClick={onPortfolioHandler}>
+                    <div id="portfolio" onClick={onPortfolioHandler}>
                         <span>Portfolio</span>
                     </div>
                 </Carousel>
+                <div style={{marginTop:'40px'}}>
+                    <span className="title">사용가능 언어 및 기술</span><br/><br/>
+                    <Row gutter={[16,16]}>
+                        <Col span={4}>
+                            <div className="itskillimg" id="javascript"/>
+                        </Col>
+                        <Col span={4}>
+                            <div className="itskillimg" id="html"/>
+                        </Col>
+                        <Col span={4}>
+                            <div className="itskillimg" id="css"/>
+                        </Col>
+                        <Col span={4}>
+                            <div className="itskillimg" id="java"/>
+                        </Col>
+                        <Col span={4}>
+                            <div className="itskillimg" id="mysql"/>
+                        </Col>
+                        <Col span={4}>
+                            <div className="itskillimg" id="mongodb"/>
+                        </Col>
+                    </Row>
+                    <Row gutter={[16,16]}>
+                        <Col span={4}>
+                            <div className="itskillimg" id="node"/>
+                        </Col>
+                        <Col span={4}>
+                            <div className="itskillimg" id="express"/>
+                        </Col>
+                        <Col span={4}>
+                            <div className="itskillimg" id="react"/>
+                        </Col>
+                        <Col span={4}>
+                            <div className="itskillimg" id="redux"/>
+                        </Col>
+                        <Col span={4}>
+                            <div className="itskillimg" id="spring"/>
+                        </Col>
+                        <Col span={4}>
+                        </Col>
+                    </Row>
+                </div>
             </div>
         </div>
     )
--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -1,22 +1,35 @@
 import React from 'react'
-import { Menu } from 'antd';
+import { Menu, Row, Col } from 'antd';
+import {BarsOutlined} from '@ant-design/icons';
+import './NavBar.css';
+const { SubMenu } = Menu;
 
 function NavBar() {
     return (
         <div style={{ width: '100%', margin: '0' }}>
             <div style={{ width: '85%', margin: '1rem auto' }}>
-                <Menu mode="horizontal"
-                    style={{ fontSize: "24px", fontFamily: 'Roboto' }}>
-                    <Menu.Item key="home">
-                        <a href="/">HOME</a>
-                    </Menu.Item>
-                    <Menu.Item key="profile">
-                        <a href="/">Profile</a>
-                    </Menu.Item>
-                    <Menu.Item key="portfolio">
-                        <a href="/">Portfolio</a>
-                    </Menu.Item>
-                </Menu>
+                <Row gutter={16}>
+                    <Col span={8}>
+                        <Menu mode="horizontal" style={{ borderBottom: 'none' }}>
+                            <SubMenu icon={<BarsOutlined style={{ fontSize: "24px" }} />}>
+                                <Menu.Item key="home">
+                                    <a href="/">HOME</a>
+                                </Menu.Item>
+                                <Menu.Item key="profile">
+                                    <a href="/">Profile</a>
+                                </Menu.Item>
+                                <Menu.Item key="portfolio">
+                                    <a href="/">Portfolio</a>
+                                </Menu.Item>
+                            </SubMenu>
+                        </Menu>
+                    </Col>
+                    <Col span={8} style={{textAlign:'center'}}>
+                        <div className="title">JJK Portfolio Site</div>
+                    </Col>
+                    <Col span={8}>
+                    </Col>
+                </Row>
             </div>
         </div>
     )
