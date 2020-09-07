20cf4f27f9e6321ee9ebd3900f9159f40c2043dc
jeonjaekyun <jkyun01@gmail.com>
2020-09-07 14:42:38 +0900

    aboutme 수정

--- src/components/views/LandingPage/LandingPage.js
+++ src/components/views/LandingPage/LandingPage.js
@@ -26,11 +26,10 @@ function LandingPage(props) {
                         <hr style={{ border: '1.5px solid orange' }} />
                     </div>
                     <p className="aboutme">
-                    Javascript를 통해 Nodejs와 React를 개발하는 신입 개발자입니다.
-                    웹 개발은 Java를 활용한 Spring Framework로 시작하였지만 Frontend를 공부해 보기 위해
-                    React를 공부하다가 Nodejs에 대해서 알게 되고 공부하게 되었습니다. 이러한 과정을 통해
-                    Javascript를 사용하면 Back, Front 심지어 MongoDB 같은 DB도 
-                    다룰 수 있다는 편리함과 확장성이 있다는 것에 재미를 느끼며 학습하고 있습니다.
+                    Javascript를 통해 Nodejs와 React를 개발하는 신입 개발자입니다. 
+                    웹 개발은 Java를 활용한 Spring Framework로 시작하였지만 Frontend를 공부하기 위해 React를 접하게 되었고 
+                    나아가 Nodejs를 접하게 되었습니다. 이러한 과정을 통해 Javascript를 사용하면 Back, Front 심지어 
+                    MongoDB 같은 DB도 다룰 수 있다는 편리함과 확장성이 있다는 것에 재미를 느끼며 학습하고 있습니다.
                     </p>
                 </div>
                 <div style={{ marginTop: '40px' }}>
